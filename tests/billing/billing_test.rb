0c38f5a8104658e276e2aafbd0666013c7c7ab62
Alex <alexander.zhukovetsky@onapp.com>
2014-10-22 17:15:22 +0300

    Add scenary fot Billing Plan.

---
+++ tests/billing/billing_test.rb
@@ -0,0 +1,38 @@
+# Test for checking Billing Plan functionality.
+#
+# describe "Checking Billing Plan functionality" do
+  it "Create Billin Plan with empty 'Label'" do
+    pass
+  end
+
+  it "Create Billing Plan with negative price value" do
+    pass
+  end
+
+  it "Create Billin Plan with unexisted currency" do
+    pass
+  end
+
+  it "Create Billing Plan with correct values" do
+    pass
+  end
+
+   it "Edit Billin Plan with empty 'Label'" do
+    pass
+  end
+
+  it "Edit Billing Plan with negative price value" do
+    pass
+  end
+
+  it "Edit Billin Plan with unexisted currency" do
+    pass
+  end
+
+  it "Edit Billing Plan with correct values" do
+    pass
+  end
+
+  it "Delete Billing Plan" do
+    pass
+  end
