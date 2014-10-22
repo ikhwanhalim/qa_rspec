a42d09f7d5842973c8f51d15c8712f1bc0bce952
Roman <roman.lazoryshchak@onapp.com>
2014-10-22 17:18:57 +0300

    Fix Curl

--- lib/helpers/template_manager.rb
+++ lib/helpers/template_manager.rb
@@ -1,19 +1,21 @@
-require_relative 'onapp_http'
+require_relative 'curl'
 
 module TemplateManager
-  include OnappHTTP
+  include Curl
+  include Parser
+  attr_accessor :template
 
   def get_available
-    get("#{@ip}/templates/available.json")
+    from_api get("/templates/available")
   end
 
   def get_all
-    result = get("#{@ip}/templates/all.json")
+    result = from_api get("/templates/all")
     result.map {|t| t["image_template"]["manager_id"]}
   end
 
   def download(id)
     data = {image_template: {manager_id: id}}
-    post("#{@ip}/templates.json", data) unless get_all.include?(id)
+    @template = get_hash_with_split(post("/templates", data.to_json), "image_template") unless get_all.include?(id)
   end
 end
