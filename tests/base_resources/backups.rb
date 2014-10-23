efad8aa27f12936705e68650eeaf64b794e570cf
Alex <alexander.zhukovetsky@onapp.com>
2014-10-23 09:26:46 +0300

    Add description for "User VS limit" tests.

---
+++ tests/base_resources/backups.rb
@@ -0,0 +1,34 @@
+# Check Backups
+
+describe "Check Backups Limits" do
+  # Check 'Free' limits
+  it "Create with negative 'Free' value" do 
+    pass
+  end
+  it "Create with pozitive 'Free' value > 0" do 
+    pass
+  end
+  it "Edit 'Free' value, set 0" do
+    pass
+  end
+  # Check 'Max' limits
+  it "Create with negative 'Max' value" do 
+    pass
+  end
+  it "Create with pozitive 'Max' value > 0" do 
+    pass
+  end
+  it "Edit 'Max' value, set 0 (Unlimited)" do
+    pass
+  end
+  # Check 'Prices'
+  it "Create with negative 'Price' value" do 
+    pass
+  end
+  it "Create with pozitive 'Price' value > 0" do 
+    pass
+  end
+  it "Edit 'Price' value, set 0" do
+    pass
+  end
+end
---
+++ tests/base_resources/virtual_server.rb
@@ -0,0 +1,13 @@
+# Check resources for Virtual Server
+
+describe "Check resources for Virtual Server" do
+  it "Create with negative 'Max' value" do 
+    pass
+  end
+  it "Create with pozitive 'Max' value > 0" do 
+    pass
+  end
+  it "Edit 'Max' value, set 0 (Unlimited)" do
+    pass
+  end
+end
