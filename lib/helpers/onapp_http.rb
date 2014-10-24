d9c54857f5ac60a6cfa3b9abc4314725b046dc56
Roman <roman.lazoryshchak@onapp.com>
2014-10-24 17:06:27 +0300

    Add PUT method

--- lib/helpers/onapp_http.rb
+++ lib/helpers/onapp_http.rb
@@ -7,24 +7,32 @@ module OnappHTTP
 
   def auth(url, user, pass)
     @conn = Mechanize.new
+    @headers = {'Accept' => 'application/json','Content-Type' => 'application/json'}
     @conn.add_auth(url, user, pass)
   end
 
   def get(url)
-    JSON.parse @conn.get(url).body
+    JSON.parse @conn.get(url, @headers).body
     rescue Mechanize::ResponseCodeError => e
       JSON.parse e.page.body
   end
 
   def post(url, data="")
-    request = @conn.post(url, data.to_json, {'Content-Type' => 'application/json'})
+    request = @conn.post(url, data.to_json, @headers)
     JSON.parse(request.body) unless request.body.blank?
     rescue Mechanize::ResponseCodeError => e
       JSON.parse e.page.body
   end
 
   def delete(url)
-    @conn.delete(url)
+    @conn.delete(url, @headers)
+    rescue Mechanize::ResponseCodeError => e
+      JSON.parse e.page.body
+  end
+
+  def put(url, data="")
+    request = @conn.put(url, data.to_json, @headers)
+    JSON.parse(request.body) unless request.body.blank?
     rescue Mechanize::ResponseCodeError => e
       JSON.parse e.page.body
   end
