29d8c28638bf6f73b5ac6321750e90a5b3e25ea6
Roman <roman.lazoryshchak@onapp.com>
2014-10-24 18:08:56 +0300

    Federation tests

--- lib/onapp_trader.rb
+++ lib/onapp_trader.rb
@@ -1,3 +1,52 @@
-class OnappTrader
+require 'yaml'
+require 'helpers/onapp_http'
 
+class OnappTrader
+  include OnappHTTP
+  attr_accessor :subscribed_zone
+
+  def initialize
+    data = YAML::load_file('config/conf.yml')
+    data['trader'].each do |k, v|
+      instance_variable_set("@#{k}",v)
+      eigenclass = class<<self; self; end
+      eigenclass.class_eval do
+        attr_accessor k
+      end
+    end
+    auth "#{@url}/users/sign_in", @user, @pass
+  end
+
+  def subscribe(federation_id)
+    #all_unsubscribed.select {|z| z['federation_id'] == federation_id}.first
+    data = {'hypervisor_zone_namer' =>
+                {'hypervisor_group_label' => federation_id,
+                 'hypervisor_label' => federation_id,
+                 'data_store_group_label' => federation_id,
+                 'data_store_label' => federation_id,
+                 'network_group_label' => federation_id,
+                 'network_label' => federation_id,
+                 'image_template_group_label' => federation_id,
+                }
+    }
+    post("#{@url}/federation/hypervisor_zones/#{federation_id}/subscribe.json", data)
+    @subscribed_zone = all_subscribed.select {|z| z['federation_id'] == federation_id}.first
+  end
+
+  def all_unsubscribed
+    zones = get "#{@url}/federation/hypervisor_zones/unsubscribed.json"
+    zones.map! { |z| z["hypervisor_zone"]}
+  end
+
+  def all_subscribed
+    zones = get "#{@url}/settings/hypervisor_zones.json"
+    zones.map! { |z| z["hypervisor_group"]}
+    zones.delete_if { |z| z['federation_id'] == nil }
+  end
+
+  def unsubscribe_all
+    all_subscribed.each do |z|
+      delete("#{@url}/federation/hypervisor_zones/#{z['id']}/unsubscribe.json")
+    end
+  end
 end
--- market_tests/federation_spec.rb
+++ market_tests/federation_spec.rb
@@ -1,16 +1,30 @@
 require 'onapp_supplier'
+require 'onapp_trader'
 
 describe "Supplier basic tests" do
   before :all do
     @supplier = OnappSupplier.new
+    @trader = OnappTrader.new
+    if @supplier.all_federated.empty?
+      @supplier.add_to_federation
+    else
+      @supplier.published_zone = @supplier.all_federated.first
+    end
   end
 
   after :all do
-    @supplier.remove_from_federation if @supplier
+    @trader.unsubscribe_all
+    @supplier.remove_all_from_federation
   end
 
   it "Add to federation" do
-    @supplier.add_to_federation
-    expect(@supplier.federation_id.empty?).to be_false
+    @supplier.published_zone["federation_enabled"].should be_true
+    @supplier.published_zone["federation_id"].should be_true
+  end
+
+  it "Trader should be able subscribe to zone" do
+    @trader.subscribe(@supplier.published_zone["federation_id"])
+    @trader.subscribed_zone.should be_true
+    @trader.subscribed_zone['federation_id'].should eq @supplier.published_zone['federation_id']
   end
 end
