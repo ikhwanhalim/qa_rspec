a28470afe78c2284dde3966bbdcc7ec1cc9f4845
Roman <roman.lazoryshchak@onapp.com>
2014-10-24 11:13:15 +0300

    Add rescue to HTTP

--- lib/helpers/onapp_http.rb
+++ lib/helpers/onapp_http.rb
@@ -1,24 +1,31 @@
 require 'mechanize'
 require 'json'
+require 'active_support/all'
 
 module OnappHTTP
   attr_accessor :conn, :ip
 
-  def auth(url, user, pwd)
+  def auth(url, user, pass)
     @conn = Mechanize.new
-    @conn.add_auth(url, user, pwd)
+    @conn.add_auth(url, user, pass)
   end
 
   def get(url)
     JSON.parse @conn.get(url).body
+    rescue Mechanize::ResponseCodeError => e
+      JSON.parse e.page.body
   end
 
   def post(url, data="")
-    curl = @conn.post(url, data.to_json, {'Content-Type' => 'application/json'})
-    JSON.parse(curl.body)
+    request = @conn.post(url, data.to_json, {'Content-Type' => 'application/json'})
+    JSON.parse(request.body) unless request.body.blank?
+    rescue Mechanize::ResponseCodeError => e
+      JSON.parse e.page.body
   end
 
   def delete(url)
     @conn.delete(url)
+    rescue Mechanize::ResponseCodeError => e
+      JSON.parse e.page.body
   end
 end
