16fff967a3b244b4247b573dcf7b8bcd06511855
Roman Lazoryshchak <roman.lazoryshchak@onapp.com>
2014-11-19 13:22:28 +0200

    Find mounted disks

--- lib/helpers/scripts/debian_centos.sh
+++ lib/helpers/scripts/debian_centos.sh
@@ -8,6 +8,8 @@ disk=`df -hm | awk {'print $1" "$2'} | grep /dev/ | awk {'print $2-G-B-H'}`
 
 swap=`free -m | grep wap: | awk {'print $2-G-B-H'}`
 
+mounted_tmp=`df -hm | awk -v dd=':' -v dq='"' '/mnt/ {print dq$6dq dd dq$2dq}'`
 
+mounted=`echo $mounted_tmp | sed 's/\" \"/\"\,\"/g'`
 
-echo "{\"system\":{\"memory\":\"$memory\",\"cpu\":\"$cpu\"}, \"disks\":{\"primary\":\"$disk\", \"swap\":\"$swap\"}}"
+echo "{\"system\":{\"memory\":\"$memory\",\"cpu\":\"$cpu\"}, \"disks\":{\"primary\":\"$disk\", \"swap\":\"$swap\", \"mounted\":{$mounted}}}"
