9a1b19ce47409ca63d2e34c02070aa27cdad232c
Alex <alexander.zhukovetsky@onapp.com>
2014-11-26 16:41:06 +0200

    Refactoring.

--- tests/billing/hourly_price_test.rb
+++ tests/billing/hourly_price_test.rb
@@ -9,6 +9,46 @@ require './lib/helpers/hypervisor'
 
 include Hypervisor
 
+def vm_resources_price_on_usage(vm, hv_br_data, ds_br_data, ntw_br_data)
+  #price ON
+  cpu_price = (vm.cpus - hv_br_data[:limits][:limit_free_cpu].to_i) * hv_br_data[:prices][:price_on_cpu].to_i
+  cpu_shares_price = (vm.cpu_shares - hv_br_data[:limits][:limit_free_cpu_share].to_i) * hv_br_data[:prices][:price_on_cpu_share].to_i
+  memory_price = (vm.memory - hv_br_data[:limits][:limit_free_memory].to_i) * hv_br_data[:prices][:price_on_memory].to_i
+  total_disks_size = 0
+  vm.disks.each do |disk|
+    total_disks_size += disk['disk']['disk_size']
+  end
+  disks_price = (total_disks_size - ds_br_data[:limits][:limit_free].to_i) * ds_br_data[:prices][:price_on].to_i
+
+  total_ips = vm.ip_addresses.length
+  ip_price = (total_ips - ntw_br_data[:limits][:limit_ip_free].to_i) * ntw_br_data[:prices][:price_ip_on].to_i
+  rate_limit_price = (vm.network_interfaces.first['network_interface']['rate_limit'] - ntw_br_data[:limits][:limit_rate_free].to_i) * ntw_br_data[:prices][:price_rate_on].to_i
+
+  price_on = cpu_price + cpu_shares_price + memory_price + disks_price + ip_price + rate_limit_price
+  return price_on
+
+end
+
+def vm_resources_price_off_usage(vm, hv_br_data, ds_br_data, ntw_br_data)
+  #price OFF
+  cpu_price = (vm.cpus - hv_br_data[:limits][:limit_free_cpu].to_i)  * hv_br_data[:prices][:price_off_cpu].to_i
+  cpu_shares_price = (vm.cpu_shares - hv_br_data[:limits][:limit_free_cpu_share].to_i) * hv_br_data[:prices][:price_off_cpu_share].to_i
+  memory_price = (vm.memory - hv_br_data[:limits][:limit_free_memory].to_i) * hv_br_data[:prices][:price_off_memory].to_i
+  total_disks_size = 0
+  vm.disks.each do |disk|
+    total_disks_size += disk['disk']['disk_size']
+  end
+  disks_price = (total_disks_size - ds_br_data[:limits][:limit_free].to_i) * ds_br_data[:prices][:price_off].to_i
+
+  total_ips = vm.ip_addresses.length
+  ip_price = (total_ips - ntw_br_data[:limits][:limit_ip_free].to_i) * ntw_br_data[:prices][:price_ip_off].to_i
+  rate_limit_price = (vm.network_interfaces.first['network_interface']['rate_limit'] - ntw_br_data[:limits][:limit_rate_free].to_i) * ntw_br_data[:prices][:price_rate_off].to_i
+
+  price_off = cpu_price + cpu_shares_price + memory_price + disks_price + ip_price + rate_limit_price
+  return price_off
+
+end
+
 describe "Checking Billing Plan functionality" do
   before(:all) do
     @bp = OnappBilling.new
@@ -124,67 +164,51 @@ describe "Checking Billing Plan functionality" do
   end
 
   it 'Check hourly price' do
-    expect(@vm.price_per_hour).to eq(0.0) and expect(@vm.price_per_hour_powered_off).to eq(0.0)
+    price_on = vm_resources_price_on_usage(@vm, @hv_br_data, @ds_br_data, @ntw_br_data)
+    price_off = vm_resources_price_off_usage(@vm, @hv_br_data, @ds_br_data, @ntw_br_data)
+    expect(@vm.price_per_hour.to_i).to eq(price_on) and expect(@vm.price_per_hour_powered_off.to_i).to eq(price_off)
 
   end
 
   # Change BP
   it "Change BP to get Price for resources" do
   # HV
-    @hv_br_data = {:limits => {:limit_free_cpu => "0",
-                               :limit_cpu => "2",
-                               :limit_free_cpu_share => "0",
-                               :limit_cpu_share => "",
-                               :limit_free_memory => "0",
-                               :limit_memory => "1024"
-                   }
+    @hv_br_data[:limits] = {:limit_free_cpu => "0",
+                            :limit_cpu => "2",
+                            :limit_free_cpu_share => "0",
+                            :limit_cpu_share => "",
+                            :limit_free_memory => "0",
+                            :limit_memory => "1024"
     }
     @hv_br.edit_base_resource(@bp.bp_id, @hv_br.br_id, @hv_br_data)
 
     # DS
-    @ds_br_data = {:limits=> {:limit_free=>"0",
-                              :limit => "20",
-                              :limit_reads_completed_free => "0",
-                              :limit_data_written_free => "0",
-                              :limit_data_read_free => "0",
-                              :limit_writes_completed_free => "0"
-                   }
+    @ds_br_data[:limits] = {:limit_free=>"0",
+                            :limit => "20",
+                            :limit_reads_completed_free => "0",
+                            :limit_data_written_free => "0",
+                            :limit_data_read_free => "0",
+                            :limit_writes_completed_free => "0"
     }
     @ds_br.edit_base_resource(@bp.bp_id, @ds_br.br_id, @ds_br_data)
 
     # NW
-    @ntw_br_data = {:limits => {:limit_ip => "2",
-                                :limit_rate => "500",
-                                :limit_data_sent_free => "0",
-                                :limit_rate_free => "0",
-                                :limit_ip_free => "0",
-                                :limit_data_received_free =>"0"
-                    }
+    @ntw_br_data[:limits] = {:limit_ip => "2",
+                             :limit_rate => "500",
+                             :limit_data_sent_free => "0",
+                             :limit_rate_free => "0",
+                             :limit_ip_free => "0",
+                             :limit_data_received_free =>"0"
     }
     @ntw_br.edit_base_resource(@bp.bp_id, @ntw_br.br_id, @ntw_br_data)
   end
 
   it 'Check hourly price' do
     @vm.info_update
-    cpu_price = @vm.cpus * @hv_br_data[:prices][:price_on_cpu].to_i
-    cpu_shares_price = @vm.cpu_shares * @hv_br_data[:prices][:price_on_cpu_share].to_i
-    memory_price = @vm.memory * @hv_br_data[:prices][:price_on_memory].to_i
-    @cpu_shares
-    total_disks_size = 0
-    @vm.disks.each do |disk|
-      total_disks_size += disk['disk']['disk_size']
-    end
-    disks_price = total_disks_size * @ds_br_data[:prices][:price_on].to_i
+    price_on = vm_resources_price_on_usage(@vm, @hv_br_data, @ds_br_data, @ntw_br_data)
+    price_off = vm_resources_price_off_usage(@vm, @hv_br_data, @ds_br_data, @ntw_br_data)
 
-    total_ips = @vm.ip_addresses.length
-    ip_price = total_ips * @ntw_br_data[:prices][:price_ip_on].to_i
-    rate_limit_price = @vm.network_interfaces.first['network_interface']['rate_limit'] * @ntw_br_data[:prices][:price_rate_on].to_i
-
-    price_on = cpu_price + cpu_shares_price + memory_price + disks_price + ip_price + rate_limit_price
-
-
-    expect(@vm.price_per_hour.to_i).to eq(price_on) and expect(@vm.price_per_hour_powered_off).to eq(524.0)
+    expect(@vm.price_per_hour.to_i).to eq(price_on) and expect(@vm.price_per_hour_powered_off.to_i).to eq(price_off)
 
   end
-
 end
