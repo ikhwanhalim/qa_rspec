c8458a90698e2889663bf6eff7c368e4d7657804
Roman Lazoryshchak <roman.lazoryshchak@onapp.com>
2015-03-19 13:17:36 +0200

    Fix checking ssh port

--- lib/virtual_machine/vm_networks.rb
+++ lib/virtual_machine/vm_networks.rb
@@ -1,21 +1,25 @@
-#require 'helpers/onapp_ssh'
 require 'net/ping'
+require 'socket'
+require 'timeout'
 
 module VmNetwork
-  #include OnappSSH
-  
+
   def ssh_port_opened(network_interface = 1, ip_address_number = 1)
     ip_address = ip(network_interface, ip_address_number)
     Log.info("IP address is: #{ip_address}")
-    attempts = 12
-    while `nc -z #{ip_address} 22 -w1 || echo false`.include?('false')
-      if attempts < 0
-        return false
-      end      
-      attempts-=1
-      sleep 10
+    begin
+      Timeout::timeout(120) do
+        begin
+          s = TCPSocket.new(ip_address, 22)
+          s.close
+          return true
+        rescue Errno::ECONNREFUSED, Errno::EHOSTUNREACH
+          return false
+        end
+      end
+    rescue Timeout::Error
     end
-    true
+    return false
   end
 
   def pinged?(network_interface = 1, ip_address_number = 1)
