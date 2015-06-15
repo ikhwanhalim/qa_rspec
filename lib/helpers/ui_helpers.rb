8b3e64fc2ba8482cc71d372e0054d952452a957d
Volodymyr Pylypchuk <volodymyr.pylypchuk@onapp.com>
2015-06-15 12:37:48 +0300

    Add helpers

--- lib/helpers/ui_helpers.rb
+++ lib/helpers/ui_helpers.rb
@@ -3,7 +3,26 @@ require 'selenium-webdriver'
 
 
 module UiHelpers
-  def select_box(driver, id, value)
-      
+  def select_box(id, value)
+    browser.find_elements(:xpath => "//div[@id = '#{id}']/a/div").first.click()
+    wait_for_ajax
+    browser.find_elements(:xpath => "//div[@id = '#{id}']//input").first.send_keys(value)
+    wait_for_ajax
+    browser.find_elements(:xpath => "//div[@id = '#{id}']//li/em[contains(text(),'#{value}')]").first.click()
+    wait_for_ajax
   end
+
+  def slide_check_box(id, value)
+    if !value && browser.find_elements(:xpath => "//input[@id = '#{id}']/../div[@class='switch_handle']").any?
+      browser.find_elements(:xpath => "//input[@id = '#{id}']/..").first.click()
+    elsif value && !browser.find_elements(:xpath => "//input[@id = '#{id}']/../div[@class='switch_handle']").any?
+      browser.find_elements(:xpath => "//input[@id = '#{id}']/..").first.click()
+    end
+    wait_for_ajax
+  end
+  def box_check_box(value)
+    browser.find_elements(:xpath => "//h3[@title='#{value}']/..//input").first.click()
+    wait_for_ajax
+  end
+
 end
