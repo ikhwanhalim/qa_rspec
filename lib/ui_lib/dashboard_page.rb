7523381948604301fb1a5e698182c9747a4b3782
Roman Lazoryshchak <roman.lazoryshchak@onapp.com>
2015-06-16 10:06:33 +0300

    Add CDN status

--- lib/ui_lib/dashboard_page.rb
+++ lib/ui_lib/dashboard_page.rb
@@ -6,7 +6,6 @@ class DashboardPage
   include PageObject
   page_url YAML::load_file('config/conf.yml')['url']
 
-
   span(:alert, :xpath => '//span/span')
-
+  h3(:cdn_status, xpath: "//h3[contains(text(),'CDN')]")
 end
