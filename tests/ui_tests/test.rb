607a854b1b8e12e20f85b9e70d606881dff8b59c
Roman Lazoryshchak <roman.lazoryshchak@onapp.com>
2015-06-16 10:06:33 +0300

    Add CDN status

--- tests/ui_tests/test.rb
+++ tests/ui_tests/test.rb
@@ -14,42 +14,36 @@ describe 'basic test' do
     @home_page = @login_page.login
     @home_page.current_url.should eq("#{@base_url}/")
     @home_page.alert.should eq('Signed in successfully.')
+    fail 'CDN is not enabled' unless @home_page.cdn_status?
   end
-
 
   it 'Create CDN Resource' do
-    @add_cdn_resource_page = AddCdnResourcePage.new(@browser, true)
-
-    @add_cdn_resource_page.cdn_resource_type = 'http'
-    @add_cdn_resource_page.next_page
-
-     @add_cdn_resource_page.cdn_hostname = 'vovka.the.best'
-     @add_cdn_resource_page.content_origin = 'PULL'
-     @add_cdn_resource_page.origin1 = '10.10.10.10'
-     @add_cdn_resource_page.add_origin2
-     @add_cdn_resource_page.origin1 = 'vovka.the.best'
-     @add_cdn_resource_page.add_origin3
-     @add_cdn_resource_page.origin3 = '10.10.10.12'
-     @add_cdn_resource_page.remove_origin3
-     @add_cdn_resource_page.remove_origin2    #
-     @add_cdn_resource_page.add_origin2
-     @add_cdn_resource_page.origin2 = '10.10.10.11'
-     @add_cdn_resource_page.add_origin3
-     @add_cdn_resource_page.origin3 = '10.10.10.12'
-     @add_cdn_resource_page.next_page
-     @add_cdn_resource_page.edge_groups = 'iraEG1', 'iraEG3'
-
-
+    add_cdn_resource_page = AddCdnResourcePage.new(@browser, true)
+    add_cdn_resource_page.cdn_resource_type = 'http'
+    add_cdn_resource_page.next_page
+    add_cdn_resource_page.cdn_hostname = 'vovka.the.best'
+    add_cdn_resource_page.content_origin = 'PULL'
+    add_cdn_resource_page.origin1 = '10.10.10.10'
+    add_cdn_resource_page.add_origin2
+    add_cdn_resource_page.origin1 = 'vovka.the.best'
+    add_cdn_resource_page.add_origin3
+    add_cdn_resource_page.origin3 = '10.10.10.12'
+    add_cdn_resource_page.remove_origin3
+    add_cdn_resource_page.remove_origin2    #
+    add_cdn_resource_page.add_origin2
+    add_cdn_resource_page.origin2 = '10.10.10.11'
+    add_cdn_resource_page.add_origin3
+    add_cdn_resource_page.origin3 = '10.10.10.12'
+    add_cdn_resource_page.next_page
+    add_cdn_resource_page.edge_groups = 'iraEG1', 'iraEG3'
   end
+
   it 'Edit CDN Resources' do
-    @add_cdn_resource_page = AddCdnResourcePage.new(@browser, true)
+    add_cdn_resource_page = AddCdnResourcePage.new(@browser, true)
   end
-
-
 
   after :all do
     @browser.close
     # @headless.destroy
   end
-
 end
