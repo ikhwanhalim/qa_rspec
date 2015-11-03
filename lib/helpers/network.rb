ee2ca6b35b91dc661eb9b6857133916d07c3af9b
Roman <pinchlog1@gmail.com>
2015-11-03 22:05:31 +0200

    Fix ping

--- lib/helpers/network.rb
+++ lib/helpers/network.rb
@@ -1,7 +1,7 @@
 module Network
   include Waiter
 
-  def ssh_opened?(port = 22)
+  def port_opened?(port = 22)
     Log.info("Connect to: #{ip_address}:#{port}")
     wait_until(300) do
       begin
@@ -13,7 +13,7 @@ module Network
     end
   end
 
-  def ssh_closed?(port = 22)
+  def port_closed?(port = 22)
     Log.info("Connect to: #{ip_address}:#{port}")
     wait_until(300) do
       begin
@@ -28,22 +28,22 @@ module Network
   def pinged?
     Log.info("Ping IP address: #{ip_address}")
     wait_until(300) do
-      Net::Ping::External.new(ip_address).ping ? true : false
+      system("ping -c1 #{ip_address}")
     end
   end
 
   def not_pinged?
     Log.info("Ping IP address: #{ip_address}")
     wait_until(300) do
-      Net::Ping::External.new(ip_address).ping ? false : true
+      system("ping -c1 #{ip_address}") ? false : true
     end
   end
 
   def up?
-    pinged? && ssh_opened?
+    pinged? && port_opened?
   end
 
   def down?
-    not_pinged? && ssh_closed?
+    not_pinged? && port_closed?
   end
 end
