be3011417eb5d7a23e6fb5e6b803b2c7e94b8619
Roman Lazoryshchak <roman.lazoryshchak@onapp.com>
2015-12-07 15:36:09 +0200

    Mark base template tests

--- spec/onapp/template/release_template_spec.rb
+++ spec/onapp/template/release_template_spec.rb
@@ -20,7 +20,7 @@ describe 'Templates Tests' do
   let(:distros) { %w(rhel ubuntu) }
 
 
-  it 'Virtual server should be stopped/started' do
+  it 'Virtual server should be stopped/started', base: true do
     virtual_machine.stop
     (virtual_machine.down?).should be true
     virtual_machine.start_up
@@ -36,18 +36,18 @@ describe 'Templates Tests' do
     virtual_machine.start_up
   end
 
-  it 'Should be possible to update OS (yum/apt-get) and check boot availability' do
+  it 'Should be possible to update OS (yum/apt-get) and check boot availability', base: true do
     skip unless distros.include?(virtual_machine.operating_system_distro)
     virtual_machine.update_os
     virtual_machine.reboot
     (virtual_machine.up?).should be true
   end
 
-  it 'Make backup, convert it to template and build new VS based on new Template' do
+  it 'Make backup, convert it to template and build new VS based on new Template', base: true do
     #TODO
   end
 
-  it 'Hot resize fot template' do
+  it 'Hot resize fot template', base: true do
     skip
     template.db_enable_hotresize
   end
