a4bd4bd8d26de01bc03fb8dbdf194879069d3eca
Volodymyr Pylypchuk <volodymyr.pylypchuk@onapp.com>
2017-05-29 10:53:17 +0300

    Add vCenter tests for disks operations

--- groups/test_cases/vcenter_vs_test_cases.rb
+++ groups/test_cases/vcenter_vs_test_cases.rb
@@ -70,3 +70,39 @@ class VCenterVsRebuildOperation < VCenterVsTestCases
     Log.error('Virtual server not running after rebuild') if !env.virtual_machine.up?
   end
 end
+
+class VCenterVSPrimaryDiskOperations < VCenterVsTestCases
+
+  def execute
+    disk_size_on_interface = env.virtual_machine.disk.disk_size
+    disk_size_on_vm = env.virtual_machine.ssh_execute("fdisk -l | grep 'Disk' | grep 'dev' | grep -v 'mapper' | awk '{print \\$5}'").last.to_f/1024/1024/1024
+    Log.error("Disk size on VM is not correct initially, expected #{disk_size_on_interface} but got #{disk_size_on_vm}") if disk_size_on_interface != disk_size_on_vm
+    # new_size = env.virtual_machine.disk.disk_size + 5
+    # env.virtual_machine.disk.edit(disk_size: new_size)
+    # Log.error('Virtual server not running after increase primary disk size') if !env.virtual_machine.up?
+    # disk_size_on_interface = env.virtual_machine.disk.disk_size
+    # disk_size_on_vm = env.virtual_machine.ssh_execute("fdisk -l | grep 'Disk' | grep 'dev' | grep -v 'mapper' | awk '{print \\$5}'").last.to_f/1024/1024/1024
+    # Log.error("Disk size on VM is not correct after increase primary disk size, expected #{disk_size_on_interface} but got #{disk_size_on_vm}") if disk_size_on_interface != disk_size_on_vm
+    # VMWare do not support decrease disk size
+  end
+end
+
+class VCenterVSAdditionalDiskOperations < VCenterVsTestCases
+
+  def execute
+     env.virtual_machine.add_disk
+     env.virtual_machine.disk('additional').wait_for_build
+     Log.error('Virtual server not running after adding') if !env.virtual_machine.up?
+     disk_size_on_interface = env.virtual_machine.disk('additional').disk_size
+     disk_size_on_vm = env.virtual_machine.ssh_execute("fdisk -l | grep 'Disk' | grep 'dev' | grep -v 'mapper' | awk '{print \\$5}'").last.to_f/1024/1024/1024
+     Log.error("Additional disk size on VM is not correct initially, expected #{disk_size_on_interface} but got #{disk_size_on_vm}") if disk_size_on_interface != disk_size_on_vm
+     new_size = env.virtual_machine.disk('additional').disk_size + 5
+     env.virtual_machine.disk('additional').edit(disk_size: new_size)
+     env.virtual_machine.disk('additional').wait_for_resize
+     env.virtual_machine.disk('additional').wait_for_update_fstab
+     Log.error('Virtual server not running after increase additional disk size') if !env.virtual_machine.up?
+     disk_size_on_interface = env.virtual_machine.disk('additional').disk_size
+     disk_size_on_vm = env.virtual_machine.ssh_execute("fdisk -l | grep 'Disk' | grep 'dev' | grep -v 'mapper' | awk '{print \\$5}'").last.to_f/1024/1024/1024
+     Log.error("Additional disk size on VM is not correct after increase, expected #{disk_size_on_interface} but got #{disk_size_on_vm}") if disk_size_on_interface != disk_size_on_vm
+  end
+end
--- spec/onapp/virtual_machine/vm_build_on_vcenter.rb
+++ spec/onapp/virtual_machine/vm_build_on_vcenter.rb
@@ -1,9 +1,11 @@
 require 'spec_helper'
 require 'rspec'
-describe 'VIRTUAL MACHINE REGRESSION AUTOTEST' do
+describe 'VIRTUAL MACHINE ON VMWARE (VCENTER) REGRESSION AUTOTEST' do
+
   before :all do
     @env=VirtualServerOnVCenter.new.precondition('image')
   end
+
   after :all do
     @env.aftercondition
   end
@@ -12,21 +14,37 @@ describe 'VIRTUAL MACHINE REGRESSION AUTOTEST' do
     @env=@test.env
   end
 
-  describe 'Virtual server on vCenter Hypervisor' do
-    it 'Power Options' do
-       @test = VCenterVsPowerOptions.new(@env)
-       skip('Test case could not be executed. Some configuration do not satisfy requirements') if !@test.executable?
-       @test.execute
-     end
-    it 'Suspend Options' do
-       @test = VCenterVsSuspendOptions.new(@env)
-       skip('Test case could not be executed. Some configuration do not satisfy requirements') if !@test.executable?
-       @test.execute
-     end
-    it 'Rebuild Option' do
-       @test = VCenterVsRebuildOperation.new(@env)
-       skip('Test case could not be executed. Some configuration do not satisfy requirements') if !@test.executable?
-       @test.execute
-    end
+  it 'Power Options' do
+    @test = VCenterVsPowerOptions.new(@env)
+    skip('')
+    skip('Test case could not be executed. Some configuration do not satisfy requirements') if !@test.executable?
+    @test.execute
   end
+
+  it 'Suspend Options' do
+    @test = VCenterVsSuspendOptions.new(@env)
+    skip('Test case could not be executed. Some configuration do not satisfy requirements') if !@test.executable?
+    @test.execute
+  end
+
+  it 'Rebuild Option' do
+    skip('')
+    @test = VCenterVsRebuildOperation.new(@env)
+    skip('Test case could not be executed. Some configuration do not satisfy requirements') if !@test.executable?
+    @test.execute
+  end
+
+  it 'Primary disk operations' do
+    skip('')
+    @test = VCenterVSPrimaryDiskOperations.new(@env)
+    skip('Test case could not be executed. Some configuration do not satisfy requirements') if !@test.executable?
+    @test.execute
+  end
+
+  it 'Additional disk operations' do
+    @test = VCenterVSAdditionalDiskOperations.new(@env)
+    skip('Test case could not be executed. Some configuration do not satisfy requirements') if !@test.executable?
+    @test.execute
+  end
+
 end
